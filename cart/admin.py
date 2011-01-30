86d9587224ef294a17a7759a8924669b9396294d
Jon Renaut <jon@manfredmacx.com>
2011-01-30 12:03:28 -0500

    Add some functions, update README, clean up admin, cart_processor

--- cart/admin.py
+++ cart/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from models import Cart, Item
 
-admin.site.register(Cart)
-admin.site.register(Item)
+class CartAdmin(admin.ModelAdmin):
+	list_display = ('id', 'creation_date', 'checked_out',)
+	list_filter = ('checked_out',)
+	
+class ItemAdmin(admin.ModelAdmin):
+	list_display = ('id', 'cart', 'quantity', 'unit_price', 'object_id',)
+	
+admin.site.register(Cart, CartAdmin)
+admin.site.register(Item, ItemAdmin)
